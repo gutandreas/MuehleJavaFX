e4aae6d1b56a7a8ee6fd052b3625561065377dc7
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2022-01-20 22:02:37 +0100

    Makes class Position immutable

--- src/main/java/edu/andreasgut/game/Position.java
+++ src/main/java/edu/andreasgut/game/Position.java
@@ -3,10 +3,7 @@ package edu.andreasgut.game;
 
 public class Position implements Comparable<Position> {
 
-    private int ring, field;
-
-    public Position() {
-    }
+    private final int ring, field;
 
     public Position(int ring, int field) {
         this.ring = ring;
@@ -21,20 +18,15 @@ public class Position implements Comparable<Position> {
         return field;
     }
 
-    public void setRing(int ring) {
-        this.ring = ring;
-    }
-
-    public void setField(int field) {
-        this.field = field;
-    }
-
     @Override
-    public boolean equals(Object o){
-        Position position = (Position) o;
-        return ring == position.getRing() && field == position.getField();
+    public boolean equals(Object o) {
+    	if(o instanceof Position) {
+    		Position position = (Position) o;
+    		return ring == position.getRing() && field == position.getField();
+    	} else {
+    		return false;
+    	}
     }
-
 
     @Override
     public int compareTo(Position position) {
