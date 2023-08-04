ade226fd450b67d18bb03612b3aff5337487a0bb
Andreas Gut <andreasgut@gmx.ch>
2023-08-04 21:51:35 +0200

    Exercise zu Baum angepasst (Lösung entfernt, Beschreibung hinzugefügt)

--- src/main/java/edu/andreasgut/exercises/Tree.java
+++ src/main/java/edu/andreasgut/exercises/Tree.java
@@ -9,14 +9,20 @@ public class Tree {
         this.rootNode = rootNode;
     }
 
+    // TODO: Implementiere die Methode so, dass ein Kindknoten an die passende Stelle im Baum eingefügt wird.
+    //  Vergiss dabei nicht, dass nicht nur der Kind- mit dem Elternknoten verknüpft werden muss,
+    //  sondern auch die umgekehrte Verknüpfung erstellt werden muss.
     public void addNode(TreeNode child, TreeNode parent){
 
     }
 
-    public void moveNode(TreeNode fromParent, TreeNode toParent){
+    // TODO: Implementiere die Methode so, dass ein Knoten an eine andere Stelle im Baum verschoben werden kann.
+    public void moveNode(TreeNode child, TreeNode fromParent, TreeNode toParent){
 
     }
 
+    // TODO: Implementiere die Methode so, dass sie die Anzahl Kindknoten, die direkt an einem Knoten hängen,
+    //  zurückgibt.
     public int getNumberOfChildrenOf(TreeNode treeNode){
 
         int numberOfChildren = 0;
@@ -24,22 +30,29 @@ public class Tree {
         return numberOfChildren;
     }
 
+    // TODO: Implementiere die Methode so, dass sie die Anzahl Geschwisterknoten (Konten mit demselben Elternknoten)
+    //  zurückgibt.
     public int getNumberOfSiblingsOf(TreeNode treeNode){
+
         int numberOfSiblings = 0;
 
         return numberOfSiblings;
     }
 
+    // TODO (freiwillig): Implementiere die Methode so, dass sie zurückgibt, auf welcher Ebene im Baum sich ein Knoten
+    //  befindet. Die Wurzel ist dabei die Ebene 1.
     public int getLevelOf(TreeNode treeNode){
 
-        int level = 0;
+        int level = 1;
 
         return level;
     }
 
+    // Diese Methoden nicht verändern!
     public void printTree() {
         printTree(rootNode, "", true);
     }
+
     private void printTree(TreeNode treeNode, String prefix, boolean isTail) {
         System.out.println(prefix + (isTail ? "└── " : "├── ") + treeNode.getScore());
 
--- src/test/java/TreeTest.java
+++ src/test/java/TreeTest.java
@@ -7,7 +7,7 @@ public class TreeTest {
 
     @Test
     public void testAddNode(){
-        TreeNode root = new TreeNode(0);
+        TreeNode root = new TreeNode(5);
         Tree tree = new Tree(root);
 
         TreeNode nodeA = new TreeNode(10);
@@ -22,8 +22,97 @@ public class TreeTest {
 
         tree.printTree();
 
-        Assertions.assertEquals(10, root.getChildren().get(0).getScore());
+        Assertions.assertEquals(5, root.getChildren().get(0).getParent().getScore());
+        Assertions.assertEquals(34, root.getChildren().get(1).getChildren().get(1).getScore());
+    }
 
+    @Test
+    public void testMoveNode(){
+        TreeNode root = new TreeNode(5);
+        Tree tree = new Tree(root);
+
+        TreeNode nodeA = new TreeNode(10);
+        TreeNode nodeB = new TreeNode(89);
+        TreeNode nodeC = new TreeNode(100);
+        TreeNode nodeD = new TreeNode(34);
+
+        tree.addNode(nodeA, root);
+        tree.addNode(nodeB, root);
+        tree.addNode(nodeC, nodeB);
+        tree.addNode(nodeD, nodeB);
+
+        tree.moveNode(nodeD, nodeB, nodeA);
+
+        tree.printTree();
+
+        Assertions.assertEquals(nodeD, root.getChildren().get(0).getChildren().get(0));
+        Assertions.assertEquals(1, root.getChildren().get(1).getChildren().size());
+    }
+
+    @Test
+    public void testNumberOfChildrenOf(){
+        TreeNode root = new TreeNode(5);
+        Tree tree = new Tree(root);
+
+        TreeNode nodeA = new TreeNode(10);
+        TreeNode nodeB = new TreeNode(89);
+        TreeNode nodeC = new TreeNode(100);
+        TreeNode nodeD = new TreeNode(34);
+
+        tree.addNode(nodeA, root);
+        tree.addNode(nodeB, root);
+        tree.addNode(nodeC, root);
+        tree.addNode(nodeD, nodeC);
+
+
+        tree.printTree();
+
+        Assertions.assertEquals(3, tree.getNumberOfChildrenOf(root));
+        Assertions.assertEquals(1, tree.getNumberOfChildrenOf(nodeC));
+    }
+
+    @Test
+    public void testNumberOfSiblingsOf(){
+        TreeNode root = new TreeNode(5);
+        Tree tree = new Tree(root);
+
+        TreeNode nodeA = new TreeNode(10);
+        TreeNode nodeB = new TreeNode(89);
+        TreeNode nodeC = new TreeNode(100);
+        TreeNode nodeD = new TreeNode(34);
+
+        tree.addNode(nodeA, root);
+        tree.addNode(nodeB, root);
+        tree.addNode(nodeC, root);
+        tree.addNode(nodeD, nodeC);
+
+
+        tree.printTree();
+
+        Assertions.assertEquals(2, tree.getNumberOfSiblingsOf(nodeA));
+        Assertions.assertEquals(0, tree.getNumberOfChildrenOf(nodeD));
+    }
+
+    @Test
+    public void testGetLevelOf(){
+        TreeNode root = new TreeNode(5);
+        Tree tree = new Tree(root);
+
+        TreeNode nodeA = new TreeNode(10);
+        TreeNode nodeB = new TreeNode(89);
+        TreeNode nodeC = new TreeNode(100);
+        TreeNode nodeD = new TreeNode(34);
+
+        tree.addNode(nodeA, root);
+        tree.addNode(nodeB, root);
+        tree.addNode(nodeC, root);
+        tree.addNode(nodeD, nodeC);
+
+        tree.printTree();
+
+        Assertions.assertEquals(1, tree.getLevelOf(root));
+        Assertions.assertEquals(2, tree.getLevelOf(nodeA));
+        Assertions.assertEquals(3, tree.getLevelOf(nodeD));
     }
 
 }
