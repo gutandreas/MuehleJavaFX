d2493fb2318191a422c21ce01d3f211283666f18
Andreas Gut <andreasgut@gmx.ch>
2023-08-04 15:35:10 +0200

    Exercises hinzugefügt

---
+++ src/main/java/edu/andreasgut/exercises/NumberOfStones.java
@@ -0,0 +1,30 @@
+package edu.andreasgut.exercises;
+
+import edu.andreasgut.game.Board;
+import edu.andreasgut.game.Position;
+
+public class NumberOfStones {
+
+    private Board board;
+
+    public NumberOfStones(Board board) {
+        this.board = board;
+    }
+
+    //TODO: Implementiere die Methode countStonesOf() so,
+    //      dass die Anzahl des Spielers mit dem übergebenen Index zurückgegeben wird
+    //      Der Playerindex des ersten Spielers ist 0, der Index des zweiten Spielers ist 1
+
+    public int numberOfStonesOf(int playerIndex){
+
+        int counter = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board.isThisMyStone(new Position(i, j), playerIndex)) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/NumberOfStonesTest.java
@@ -0,0 +1,27 @@
+import edu.andreasgut.exercises.NumberOfStones;
+import edu.andreasgut.game.BoardImpl;
+import edu.andreasgut.game.Position;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NumberOfStonesTest {
+
+    @Test
+    public void testNumberOfStones(){
+        BoardImpl board = new BoardImpl();
+        board.putStone(new Position(0,0), 0);
+        board.putStone(new Position(2, 2), 0);
+        board.putStone(new Position(2, 4), 0);
+        board.putStone(new Position(2, 7), 0);
+        board.putStone(new Position(0,1), 1);
+        board.putStone(new Position(1, 2), 1);
+        board.putStone(new Position(2, 6), 1);
+
+
+        NumberOfStones numberOfStones = new NumberOfStones(board);
+
+        Assertions.assertEquals(4, numberOfStones.numberOfStonesOf(0));
+        Assertions.assertEquals(3, numberOfStones.numberOfStonesOf(1));
+    }
+
+}
