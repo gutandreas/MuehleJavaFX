c151cd49ecb4c1a88d4c2b149f86697c020fb8b5
Andreas Gut <andreasgut@gmx.ch>
2023-09-15 12:09:55 +0200

    Repetition hinzugefügt (Lösungen gelöscht)

--- src/main/java/edu/andreasgut/repetition/Repetition.java
+++ src/main/java/edu/andreasgut/repetition/Repetition.java
@@ -2,9 +2,6 @@ package edu.andreasgut.repetition;
 
 import edu.andreasgut.game.Board;
 import edu.andreasgut.game.Position;
-
-
-
 import java.util.LinkedList;
 
 public class Repetition {
@@ -13,9 +10,7 @@ public class Repetition {
     //       LinkedList<Position> mit demselben Inhalt zurückgibt
     public static LinkedList<Position> convertArrayToLinkedList(Position[] positions) {
         LinkedList<Position> positionLinkedList = new LinkedList<Position>();
-        for (int i = 0; i < positions.length; i++) {
-            positionLinkedList.add(positions[i]);
-        }
+
         return positionLinkedList;
     }
 
@@ -24,16 +19,16 @@ public class Repetition {
     //       die Methode pollLast(), die auf LinkedLists angewandt werden kann.
     public static LinkedList<Position> reverseListOrder(LinkedList<Position> linkedList) {
         LinkedList<Position> reversedList = new LinkedList<Position>();
-        while (!linkedList.isEmpty()) {
-            reversedList.add(linkedList.pollLast());
-        }
+
         return reversedList;
     }
 
     // TODO: Implementiere die folgende Methode so, dass sie eine Position aufnimmt und eine Position zurückgibt,
     //       welche auf der gegenüberliegenden Seite des Spielbretts liegt (also an der Mitte punktgespiegelt wurde).
     public static Position mirrorPositionAtTheCenter(Position position) {
-        Position mirroredPosition = new Position(position.getRing(), (position.getField() + 4) % 8);
+
+        Position mirroredPosition = null;
+
         return mirroredPosition;
     }
 
@@ -43,14 +38,6 @@ public class Repetition {
 
         LinkedList<Position> myPositions = new LinkedList<Position>();
 
-        for (int ring = 0; ring < 3; ring++){
-            for (int field = 0; field < 8; field++){
-                Position positionToCheck = new Position(ring, field);
-                if (board.isThisMyStone(positionToCheck, playerIndex)){
-                    myPositions.add(positionToCheck);
-                }
-            }
-        }
         return myPositions;
     }
 
@@ -58,15 +45,7 @@ public class Repetition {
     //       sodass sowohl die Eltern- wie auch die Kindknoten angepasst werden. Hinweis: Verwende dazu die Methode
     //       replaceChildren, die auf einem TreeNode aufgerufen werden kann.
     public static void swapTwoNodes(TreeNode node1, TreeNode node2){
-        LinkedList<TreeNode> childrenOfNode1 = node1.getChildren();
-        LinkedList<TreeNode> childrenOfNode2 = node2.getChildren();
-        TreeNode parentOfNode1 = node1.getParent();
-        TreeNode parentOfNode2 = node2.getParent();
 
-        node1.replaceChildren(childrenOfNode2);
-        node2.replaceChildren(childrenOfNode1);
-        node1.setParent(parentOfNode2);
-        node2.setParent(parentOfNode1);
     }
 
 
